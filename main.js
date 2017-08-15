119404c0239ffee7382e6dc7ab95bc1f5ccdd3ea
Ronald Kan <Ronald_kan@hotmail.com>
2017-08-15 16:04:14 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+ 
+const electron = require("electron");
+const appExpress = require("./app");
+
+const app = electron.app;
+
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('window-all-closed', function() {
+	if (process.platform != 'darwin') {
+		
+	}
+});
+
+app.on('ready', function() {
+	mainWindow = new BrowserWindow({width: 800, height: 600});
+	mainWindow.loadURL('http://127.0.0.1:3000');
+
+	mainWindow.on('closed', function() {
+		mainWindow = null;
+	});
+});
